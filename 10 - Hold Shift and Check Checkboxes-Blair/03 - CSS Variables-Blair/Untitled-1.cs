f2e9f6e4a952686d7f6f919c183caff1824fe3f0
blairbr <blair.j.brown@gmail.com>
2018-08-01 23:54:44 -0400

    Add files via upload

---
+++ 10 - Hold Shift and Check Checkboxes-Blair/03 - CSS Variables-Blair/Untitled-1.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        string[] inputs = Console.ReadLine().Split(' ');
+
+        int A = int.Parse(inputs[0]);
+        int B = int.Parse(inputs[1]);
+        
+        // Write an action using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+
+
+
+
+
+
+        Console.WriteLine(answer);
